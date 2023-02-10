5a2b539698efca11c865f1f49d8944c29e0a8e42
JiseongHah <jiseong1234@yonsei.ac.kr>
2023-02-10 21:19:05 +0900

    initial

---
+++ musicVaeModels.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+import torch.nn as nn
+
+class musicVaeEncoder(nn.Module):
+    def __init__(self):
+        super(musicVaeEncoder,self).__init__()
+
+
+
+    def forward(self,x):
+
+        out = x
+
+        return out
+
+
+
+
